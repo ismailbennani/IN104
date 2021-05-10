c1de7b3460f6dc2b58240287a9464d602b98d605
Mayeul CHAVANNE <mayeulchavanne@MacBook-Pro-de-Mayeul-2.local>
2021-05-11 00:50:11 +0200

    premier changement de base

--- projet/simulator/graphics/camera.py
+++ projet/simulator/graphics/camera.py
@@ -1,4 +1,5 @@
 from ..utils.vector import Vector2
+from math import tan , sin
 
 
 class Camera:
@@ -8,7 +9,10 @@ class Camera:
         self.scale = 1
 
     def to_screen_coords(self, position):
-        """ Converts the world-coordinate position to a screen-coordinate. """
+        """ Converts the world-coordinate position to screen-coordinate. """
+        theta = 30
+        self.image = Vector2( position.getx, -1/(tan(theta))*position.getx + position.gety/sin(theta) )
+        return image
         raise NotImplementedError
 
     def from_screen_coords(self, position):
