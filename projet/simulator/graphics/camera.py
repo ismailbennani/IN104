118bb76048f83d4c4dcb64448ebed9bdee5446ca
Mayeul CHAVANNE <mayeulchavanne@natalyc.logement.ensta.fr>
2021-06-02 18:32:29 +0200

    camera

--- projet/simulator/graphics/camera.py
+++ projet/simulator/graphics/camera.py
@@ -2,7 +2,7 @@ from ..utils.vector import Vector2
 from math import tan , sin, cos
 
 
-class Camera:
+class ICamera:
     def __init__(self, screen_size):
         self.screen_size = screen_size
         self.position = Vector2(0, 0)
@@ -10,14 +10,22 @@ class Camera:
 
     def to_screen_coords(self, position):
         """ Converts the world-coordinate position to screen-coordinate. """
-        theta = 30
-        self.image = Vector2( position.getx + cos(thta)*position.gety, sin(theta)*position.gety )
-        return image
         raise NotImplementedError
 
+    def from_screen_coords(self, position):
+        """ Converts the screen-coordinate position to a world-coordinate. """
+        raise NotImplementedError
+
+class Camera(ICamera):
+    
+    def to_screen_coords(self, position):
+        """ Converts the world-coordinate position to screen-coordinate. """
+        theta = 30
+        image = Vector2( position.getx + cos(thta)*position.gety, sin(theta)*position.gety )
+        return image
+    
     def from_screen_coords(self, position):
         """ Converts the screen-coordinate position to a world-coordinate. """
         theta = 30
         self.image = Vector2( position.getx -position.gety/tan(theta) , position.gety/sin(theta) )
         return image
-        raise NotImplementedError
